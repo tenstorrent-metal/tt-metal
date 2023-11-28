a009e1c3b18ba403dc1d18d5594a87f42127e721
Austin Ho <aho@tenstorrent.com>
2023-11-28 16:58:11 +0000

    #0: Embeddings

--- tt_eager/tt_dnn/op_library/embeddings/kernels/dataflow/embeddings_tilize.cpp
+++ tt_eager/tt_dnn/op_library/embeddings/kernels/dataflow/embeddings_tilize.cpp
@@ -4,9 +4,6 @@
 
 #include "dataflow_api.h"
 
-
-#define CB_ID 0
-
 void kernel_main() {
     std::uint32_t input_offset = get_arg_val<uint32_t>(0);
     std::uint32_t num_blocks      = get_arg_val<uint32_t>(1);
@@ -17,16 +14,16 @@ void kernel_main() {
     #define in_is_dram get_compile_time_arg_val(0) == 1
     #define weights_is_dram get_compile_time_arg_val(1) == 1
     constexpr uint32_t stick_size = get_compile_time_arg_val(2);
-    constexpr uint32_t input_l1_addr      = get_compile_time_arg_val(3);
-    constexpr uint32_t tiles_per_block      = get_compile_time_arg_val(4);
-    #define weight_stick_size_is_power_of_two get_compile_time_arg_val(5) == 1
+    constexpr uint32_t tiles_per_block      = get_compile_time_arg_val(3);
+    #define weight_stick_size_is_power_of_two get_compile_time_arg_val(4) == 1
 
+    const uint32_t input_page_size = sizeof(uint32_t);
 
     const InterleavedAddrGen<in_is_dram> input = {
-        .bank_base_address = input_dram_buffer_src_addr, .page_size = sizeof(uint32_t)};
+        .bank_base_address = input_dram_buffer_src_addr, .page_size = input_page_size};
 
     #if (weight_stick_size_is_power_of_two)
-    constexpr uint32_t log_base_2_of_page_size = get_compile_time_arg_val(6);
+    constexpr uint32_t log_base_2_of_page_size = get_compile_time_arg_val(5);
     const InterleavedPow2AddrGen<weights_is_dram> weights = {
         .bank_base_address = weights_dram_buffer_src_addr,
         .log_base_2_of_page_size = log_base_2_of_page_size // TODO(AP): refactor
@@ -38,6 +35,8 @@ void kernel_main() {
     };
     #endif
 
+    constexpr uint32_t cb_id_in0 = 0;
+    constexpr uint32_t cb_id_in1 = 1;
 
     constexpr uint32_t tile_height = 32;
 
@@ -45,10 +44,14 @@ void kernel_main() {
 
     uint64_t base_src_noc_addr[tile_height];
 
+    cb_reserve_back(cb_id_in1, 1);
+    uint32_t input_l1_addr = get_write_ptr(cb_id_in1);
+
+    volatile tt_l1_ptr uint32_t* input_l1_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(input_l1_addr);
 
     auto read_tiles = [&] (const uint32_t& num_tiles, const uint32_t& width_size) {
-        cb_reserve_back(CB_ID, num_tiles);
-        uint32_t l1_write_addr = get_write_ptr(CB_ID);
+        cb_reserve_back(cb_id_in0, num_tiles);
+        uint32_t l1_write_addr = get_write_ptr(cb_id_in0);
         for (uint32_t k = 0; k < tile_height; k++) {
             uint64_t src_noc_addr = base_src_noc_addr[k];
             noc_async_read(src_noc_addr, l1_write_addr, width_size);
@@ -57,17 +60,16 @@ void kernel_main() {
 
 
         noc_async_read_barrier();
-        cb_push_back(CB_ID, num_tiles);
+        cb_push_back(cb_id_in0, num_tiles);
     };
-
 
     for (uint32_t i = 0; i < num_blocks; i++) {
         for(uint32_t j=0; j < tile_height; j++){
             auto noc_input_src_addr = get_noc_addr(j+input_offset+index, input);
-            noc_async_read(noc_input_src_addr, input_l1_addr, sizeof(uint32_t));
+            noc_async_read(noc_input_src_addr, input_l1_addr, input_page_size);
             noc_async_read_barrier();
 
-            uint32_t row = ((uint32_t *)input_l1_addr)[0];
+            uint32_t row = input_l1_ptr[0];
             auto noc_src_addr = get_noc_addr(row, weights);
             base_src_noc_addr[j] = noc_src_addr;
         }
