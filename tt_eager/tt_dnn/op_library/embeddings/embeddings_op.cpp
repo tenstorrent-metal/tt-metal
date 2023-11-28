f34c2ac864adc02c72b5a85aa690c166a16380ea
Austin Ho <aho@tenstorrent.com>
2023-11-28 23:45:23 +0000

    #0: Update embeddings to have indices on last dim. TODO: Update RM variant and handle larger W cases which don't fit in L1

--- tt_eager/tt_dnn/op_library/embeddings/embeddings_op.cpp
+++ tt_eager/tt_dnn/op_library/embeddings/embeddings_op.cpp
@@ -42,27 +42,27 @@ operation::ProgramWithCallbacks embeddings_tilized(
     ////////////////////////////////////////////////////////////////////////////
     Program program{};
 
-    uint32_t num_tiles_per_cb = 1;
     bool in0_is_dram = a.buffer()->buffer_type() == tt_metal::BufferType::DRAM ? 1 : 0;
     bool weights_is_dram = weights.buffer()->buffer_type() == tt_metal::BufferType::DRAM ? 1 : 0;
-    bool weights_dtype_is_bfloat16 = weights.dtype() == tt::tt_metal::DataType::BFLOAT16;
     bool out_is_dram = output.buffer()->buffer_type() == tt_metal::BufferType::DRAM ? 1 : 0;
 
-    uint32_t last_dim = 3;
-    uint32_t element_size_in_bytes = weights.element_size();  // size of float
+    uint32_t input_element_size_bytes = a.element_size();
+    uint32_t weights_element_size_bytes = weights.element_size();
 
     // row major, page size is last dim
-    uint32_t single_page_size = weights.shape()[last_dim] * element_size_in_bytes;
+    uint32_t input_page_size = a.shape()[-1] * input_element_size_bytes;
+    uint32_t weight_page_size = weights.shape()[-1] * weights_element_size_bytes;
 
     // weights shape is [1, 1, num_embeddings, num_dim]
-    uint32_t num_embeddings = weights.shape()[last_dim - 1];
+    uint32_t num_embeddings = weights.shape()[-2];
 
     uint32_t batch_size = a.shape()[0];
-    uint32_t num_output_rows_per_batch = a.shape()[last_dim - 1];
+    uint32_t num_output_rows_per_batch = a.shape()[-1];
     uint32_t num_output_rows = num_output_rows_per_batch * batch_size;
     uint32_t num_blocks = num_output_rows / TILE_HEIGHT;
+    uint32_t num_blocks_per_batch = num_output_rows_per_batch / TILE_HEIGHT;
 
-    auto num_embedding_dims = weights.shape()[3];
+    auto num_embedding_dims = weights.shape()[-1];
 
     // setup problem and grid size
     uint32_t start_core_x = 0;
@@ -75,67 +75,68 @@ operation::ProgramWithCallbacks embeddings_tilized(
         problem_size = num_blocks;
     }
 
-    uint32_t embedding_risc_cores_per_tensix = 1;
-
     auto compute_with_storage_grid_size = device->compute_with_storage_grid_size();
     uint32_t num_cores_x = compute_with_storage_grid_size.x;
     uint32_t num_cores_y = compute_with_storage_grid_size.y;
     auto [num_cores, all_cores, core_group_1, core_group_2, num_blocks_per_core_group_1, num_blocks_per_core_group_2] =
         split_work_to_cores(compute_with_storage_grid_size, problem_size);
-
-    std::vector<tt_metal::DataMovementProcessor> risc_procs = {
-        tt_metal::DataMovementProcessor::RISCV_0, tt_metal::DataMovementProcessor::RISCV_1};
-    std::vector<tt_metal::NOC> noc_ports = {tt_metal::NOC::RISCV_0_default, tt_metal::NOC::RISCV_1_default};
+    uint32_t g1_numcores = core_group_1.num_cores();
+    uint32_t g2_numcores = core_group_2.num_cores();
 
     // Create Buffers
+    uint32_t num_tiles_per_block = weights.shape()[-1] / TILE_WIDTH;
+    tt::DataFormat input_cb_data_format = tt_metal::datatype_to_dataformat_converter(a.dtype());
 
-    uint32_t num_tiles_per_block = weights.shape()[3] / TILE_WIDTH;
-    tt::DataFormat input_cb_data_format = tt_metal::datatype_to_dataformat_converter(weights.dtype());
-    uint32_t input_single_tile_size = tt_metal::detail::TileSize(input_cb_data_format);
+    tt::DataFormat weights_cb_data_format = tt_metal::datatype_to_dataformat_converter(weights.dtype());
+    uint32_t weights_single_tile_size = tt_metal::detail::TileSize(weights_cb_data_format);
     tt::DataFormat output_cb_data_format = tt_metal::datatype_to_dataformat_converter(output.dtype());
     uint32_t output_single_tile_size = tt_metal::detail::TileSize(output_cb_data_format);
 
     uint32_t src0_cb_index = 0;
     tt_metal::CircularBufferConfig cb_src0_config =
         tt_metal::CircularBufferConfig(
-            num_tiles_per_block * input_single_tile_size, {{src0_cb_index, input_cb_data_format}})
-            .set_page_size(src0_cb_index, input_single_tile_size);
+            2 * num_tiles_per_block * weights_single_tile_size, {{src0_cb_index, weights_cb_data_format}})
+            .set_page_size(src0_cb_index, weights_single_tile_size);
     auto cb_src0 = tt_metal::CreateCircularBuffer(program, all_cores, cb_src0_config);
 
     uint32_t src1_cb_index = 1;
-    tt::DataFormat index_cb_data_format = tt_metal::datatype_to_dataformat_converter(a.dtype());
-    uint32_t index_size = a.element_size();
     tt_metal::CircularBufferConfig cb_src1_config =
-        tt_metal::CircularBufferConfig(round_up_to_mul32(index_size), {{src1_cb_index, index_cb_data_format}})
-            .set_page_size(src1_cb_index, index_size);
+        tt_metal::CircularBufferConfig(TILE_HEIGHT * input_element_size_bytes, {{src1_cb_index, input_cb_data_format}})
+            .set_page_size(src1_cb_index, TILE_HEIGHT * input_element_size_bytes);
     auto cb_src1 = tt_metal::CreateCircularBuffer(program, all_cores, cb_src1_config);
 
     uint32_t output_cb_index = 16;  // output operands start at index 16
     tt_metal::CircularBufferConfig cb_output_config =
         tt_metal::CircularBufferConfig(
-            num_tiles_per_block * output_single_tile_size, {{output_cb_index, output_cb_data_format}})
+            2 * num_tiles_per_block * output_single_tile_size, {{output_cb_index, output_cb_data_format}})
             .set_page_size(output_cb_index, output_single_tile_size);
     auto cb_output = tt_metal::CreateCircularBuffer(program, all_cores, cb_output_config);
 
-    bool weight_stick_size_is_power_of_two = is_power_of_two_at_least_32(single_page_size);
-    uint32_t weight_log2_stick_size = weight_stick_size_is_power_of_two ? (std::uint32_t)log2(single_page_size) : 0;
+    bool input_stick_size_is_power_of_two = is_power_of_two_at_least_32(input_page_size);
+    uint32_t input_log2_stick_size = input_stick_size_is_power_of_two ? (std::uint32_t)log2(input_page_size) : 0;
+    bool weight_stick_size_is_power_of_two = is_power_of_two_at_least_32(weight_page_size);
+    uint32_t weight_log2_stick_size = weight_stick_size_is_power_of_two ? (std::uint32_t)log2(weight_page_size) : 0;
 
     // Create Kernels
     // reader
     std::vector<uint32_t> embedding_compile_time_args = {
         (std::uint32_t)in0_is_dram,
+        (std::uint32_t)input_stick_size_is_power_of_two,
+        (std::uint32_t)input_page_size,
+        (std::uint32_t)input_log2_stick_size,
         (std::uint32_t)weights_is_dram,
-        (std::uint32_t)single_page_size,
-        (std::uint32_t)num_tiles_per_block,
         (std::uint32_t)weight_stick_size_is_power_of_two,
-        (std::uint32_t)weight_log2_stick_size};
+        (std::uint32_t)weight_page_size,
+        (std::uint32_t)weight_log2_stick_size,
+        (std::uint32_t)num_tiles_per_block,
+        (std::uint32_t)TILE_HEIGHT * input_element_size_bytes};
 
     auto reader_kernel_id = tt_metal::CreateKernel(
         program,
         "tt_eager/tt_dnn/op_library/embeddings/kernels/dataflow/embeddings_tilize.cpp",
         all_cores,
         tt_metal::DataMovementConfig{
-            .processor = risc_procs[0], .noc = noc_ports[0], .compile_args = embedding_compile_time_args});
+            .processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default, .compile_args = embedding_compile_time_args});
 
     if (num_blocks_per_core_group_1 > 0) {
         vector<uint32_t> compute_args_1 = {
@@ -169,42 +170,36 @@ operation::ProgramWithCallbacks embeddings_tilized(
         "tt_eager/tt_dnn/kernels/dataflow/writer_unary_interleaved_start_id.cpp",
         all_cores,
         tt_metal::DataMovementConfig{
-            .processor = risc_procs[1], .noc = noc_ports[1], .compile_args = writer_compile_time_args});
+            .processor = DataMovementProcessor::RISCV_1, .noc = NOC::RISCV_1_default, .compile_args = writer_compile_time_args});
 
     uint32_t input_offset = 0;
     uint32_t weight_offset = 0;
     uint32_t tile_offset = 0;
 
     auto cores = grid_to_cores(num_cores, num_cores_x, num_cores_y, false);
-    for (const auto &core : cores) {
+    for (uint32_t i = 0; i < cores.size(); ++i) {
+        const CoreCoord &core = cores[i];
         uint32_t local_num_blocks;
 
-        uint32_t local_num_embeddings;
-        uint32_t local_weight_offset;
         uint32_t local_input_offset = input_offset;
         uint32_t local_problem_size = num_blocks_per_core_group_1;
-        bool core_0 = true;
-        if (core_group_2.core_coord_in_core_ranges(core)) {
+        if (i >= g1_numcores) {
             local_problem_size = num_blocks_per_core_group_2;
-            core_0 = false;
         }
         if (split_weights) {
-            local_weight_offset = weight_offset;
             local_input_offset = input_offset;
             local_num_blocks = num_blocks;
-            local_num_embeddings = local_problem_size * TILE_HEIGHT;
 
         } else {
             local_input_offset = input_offset;
-            local_weight_offset = weight_offset;
-            local_num_embeddings = num_embeddings;
             local_num_blocks = local_problem_size;
         }
 
         // Reader
         {
             std::vector<uint32_t> runtime_args = {
-                (std::uint32_t)local_input_offset * TILE_HEIGHT,
+                (std::uint32_t)input_offset / num_blocks_per_batch,
+                (std::uint32_t)input_offset % num_blocks_per_batch * TILE_HEIGHT * input_element_size_bytes,
                 (std::uint32_t)(local_num_blocks),
                 (std::uint32_t)a.buffer()->address(),
                 (std::uint32_t)weights.buffer()->address()};
@@ -237,8 +232,8 @@ operation::ProgramWithCallbacks embeddings_tilized(
         for (const auto &core : cores) {
             {
                 auto &runtime_args = GetRuntimeArgs(program, reader_kernel_id, core);
-                runtime_args[2] = input_dram_buffer->address();
-                runtime_args[3] = weights_dram_buffer->address();
+                runtime_args[3] = input_dram_buffer->address();
+                runtime_args[4] = weights_dram_buffer->address();
             }
 
             {
@@ -458,18 +453,18 @@ void Embeddings::validate(const std::vector<Tensor> &input_tensors) const {
 
     TT_FATAL(weights.shape()[0] == 1 && weights.shape()[1] == 1, "First two dimensions for the weights must be 1");
     if (this->tilized) {
-        TT_FATAL(a.shape()[2] % TILE_HEIGHT == 0);
+        TT_FATAL(a.shape()[3] % TILE_HEIGHT == 0);
         TT_FATAL(weights.shape()[3] % TILE_WIDTH == 0, "Number of columns in table must be factor of tile width");
     } else {
         TT_FATAL(this->output_dtype != DataType::BFLOAT8_B);
     }
-    TT_FATAL(a.shape()[1] == 1 && a.shape()[3] == 1, "Only dim 0 && 2 for the input can be non 1");
+    TT_FATAL(a.shape()[1] == 1 && a.shape()[2] == 1, "Only dim 0 && 3 for the input can be non 1");
 }
 
 std::vector<Shape> Embeddings::compute_output_shapes(const std::vector<Tensor> &input_tensors) const {
     const auto &input_tensor = input_tensors.at(0);
     const auto &weight_tensor = input_tensors.at(1);
-    auto num_output_embeddings = input_tensor.shape()[2];
+    auto num_output_embeddings = input_tensor.shape()[3];
     auto batch_num = input_tensor.shape()[0];
     auto num_embedding_dims = weight_tensor.shape()[3];
 
