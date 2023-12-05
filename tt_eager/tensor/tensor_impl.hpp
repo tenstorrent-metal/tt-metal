3c8190068134c6f498589c11e300bb798e1cde1a
tarafdarTT <ntarafdar@tenstorrent.com>
2023-12-05 03:46:10 +0000

    #3493: remove finish from tensor_impl

--- tt_eager/tensor/tensor_impl.hpp
+++ tt_eager/tensor/tensor_impl.hpp
@@ -412,8 +412,6 @@ inline void write_data_to_device_buffer(const BufferType<T>& data_to_write, Devi
     const char *TT_METAL_SLOW_DISPATCH_MODE = std::getenv("TT_METAL_SLOW_DISPATCH_MODE");
     if (TT_METAL_SLOW_DISPATCH_MODE == nullptr) {
         EnqueueWriteBuffer(*tt::tt_metal::detail::GLOBAL_CQ, *buffer, std::begin(data_to_write), false);
-        Finish(*tt::tt_metal::detail::GLOBAL_CQ);
-        std::cout << "FORCING FINISH AFTER WRITE " << std::endl;
     } else {
         auto uint32_data = pack_vec_into_uint32_vec<T>(data_to_write);
         ::detail::WriteToBuffer(*buffer, uint32_data);
