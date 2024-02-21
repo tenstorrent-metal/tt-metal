a706cfd25d0a25fb050fa0ed8757d1d0a719cd32
rtawfik01 <rtawfik@tenstorrent.com>
2024-02-21 01:05:49 +0000

    #4686: Add dst accum mode to tilize, clean up api

--- tt_eager/tt_dnn/op_library/transformer_tms/kernels/compute/transformer_group_attn_matmul.cpp
+++ tt_eager/tt_dnn/op_library/transformer_tms/kernels/compute/transformer_group_attn_matmul.cpp
@@ -152,8 +152,6 @@ void MAIN {
             } // in0_num_blocks_w
 
             // cb_intermed1 comes from reader; untilized row-major tile
-            unpack_reconfig_data_format_srca(cb_in1, cb_intermed1);
-            pack_reconfig_data_format(cb_intermed0, out_cb_id);
             cb_wait_front(cb_intermed1, out_num_tiles);
 
             cb_reserve_back(out_cb_id, out_num_tiles);
