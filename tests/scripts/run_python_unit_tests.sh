354952cbb6df9d1120bc1749f9f27563104a7659
Michael Chiou <mchiou@tenstorrent.com>
2024-03-07 14:30:18 -0800

    #0: fix incorrectly called misc shell script in test script

--- tests/scripts/run_python_unit_tests.sh
+++ tests/scripts/run_python_unit_tests.sh
@@ -11,8 +11,7 @@ if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
     ./tests/scripts/run_python_unit_test_backward_ops.sh
     ./tests/scripts/run_python_unit_test_fallback_ops.sh
     ./tests/scripts/run_python_unit_test_loss_ops.sh
-    # This doesn't exist?
-    # ./tests/scripts/run_python_unit_test_misc_ops.sh
+    ./tests/scripts/run_python_unit_test_misc.sh
     ./tests/scripts/run_python_unit_test_moreh.sh
 
     if [ "$ARCH_NAME" != "wormhole_b0" ]; then
