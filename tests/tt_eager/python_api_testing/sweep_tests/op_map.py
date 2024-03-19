23ca06f705d51a445528a6f53d3a72381c9acafc
“Nenad <npetrovic@tenstorrent.com>
2024-03-19 15:06:11 +0000

    #5589: Add softmax inplace

--- tests/tt_eager/python_api_testing/sweep_tests/op_map.py
+++ tests/tt_eager/python_api_testing/sweep_tests/op_map.py
@@ -1687,6 +1687,22 @@ op_map = {
     "ttnn-empty": {
         "tt_lib_op": ttnn_ops.empty,
         "pytorch_op": pytorch_ops.ttnn_empty,
-    },    
+    },
+    "ttnn-attention_softmax_nomask_2": {
+        "tt_lib_op": ttnn_ops.attention_softmax_nomask_2,
+        "pytorch_op": pytorch_ops.attention_softmax_nomask,
+    },
+    "ttnn-attention_softmax_2": {
+        "tt_lib_op": ttnn_ops.attention_softmax_2,
+        "pytorch_op": pytorch_ops.attention_softmax,
+    },
+    "ttnn-zeros": {
+        "tt_lib_op": ttnn_ops.zeros,
+        "pytorch_op": pytorch_ops.zeros,
+    },
+    "ttnn-zeros_like": {
+        "tt_lib_op": ttnn_ops.zeros_like,
+        "pytorch_op": pytorch_ops.zeros_like,
+    },
 }
 
