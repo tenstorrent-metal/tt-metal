e14b5748466586a4d139ea7179feedff1886d474
hschoi <hschoi4448@gmail.com>
2024-03-19 08:14:21 +0000

    #6533: ldexp backward unitest for reproducing this issue

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_ldexp.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_ldexp.py
@@ -8,19 +8,27 @@ import tt_lib
 from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_pt_tt, compare_results
 
 
+def data_gen_pt_tt(input_shapes, device, required_grad=False, val=1):
+    pt_tensor = (torch.ones(input_shapes, requires_grad=required_grad) * val).bfloat16()
+    tt_tensor = (
+        tt_lib.tensor.Tensor(pt_tensor, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+    )
+    return pt_tensor, tt_tensor
+
+
 @pytest.mark.parametrize(
     "input_shapes",
-    (
-        (torch.Size([1, 1, 32, 32])),
-        (torch.Size([1, 1, 320, 384])),
-        (torch.Size([1, 3, 320, 384])),
-    ),
+    ((torch.Size([1, 1, 32, 32])),),
 )
 def test_bw_ldexp(input_shapes, device):
-    in_data, input_tensor = data_gen_pt_tt(input_shapes, device, True)
-    other_data, other_tensor = data_gen_pt_tt(input_shapes, device, True)
+    in_data, input_tensor = data_gen_pt_tt(input_shapes, device, True, val=1)
+    other_data, other_tensor = data_gen_pt_tt(input_shapes, device, True, val=0)
 
-    grad_data, grad_tensor = data_gen_pt_tt(input_shapes, device)
+    grad_data, grad_tensor = data_gen_pt_tt(input_shapes, device, False, 1)
+
+    print("input_tensor", input_tensor)
+    print("other_tensor", other_tensor)
+    print("grad_tensor", grad_tensor)
 
     tt_output_tensor_on_device = tt_lib.tensor.ldexp_bw(grad_tensor, input_tensor, other_tensor)
 
@@ -33,4 +41,7 @@ def test_bw_ldexp(input_shapes, device):
 
     golden_tensor = [in_data.grad, other_data.grad]
     comp_pass = compare_results(tt_output_tensor_on_device, golden_tensor)
+
+    print("tt_output_tensor_on_device", tt_output_tensor_on_device)
+    print("golden_tensor", golden_tensor)
     assert comp_pass
