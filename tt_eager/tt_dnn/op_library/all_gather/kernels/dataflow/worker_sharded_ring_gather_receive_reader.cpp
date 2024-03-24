778cab6899b659714a6e4fd2579cf8936665a403
Sean Nijjar <sean.nijjar@gmail.com>
2024-03-24 16:10:51 +0000

    Revert "#6041: remove dprints"
    This reverts commit 282c95786cc57131cb0b133834c2ccf66218fe27.

--- tt_eager/tt_dnn/op_library/all_gather/kernels/dataflow/worker_sharded_ring_gather_receive_reader.cpp
+++ tt_eager/tt_dnn/op_library/all_gather/kernels/dataflow/worker_sharded_ring_gather_receive_reader.cpp
@@ -6,13 +6,15 @@
 
 #include "dataflow_api.h"
 #include "debug/assert.h"
+#include "debug/dprint.h"
 #include "tt_eager/tt_dnn/op_library/all_gather/kernels/dataflow/worker_ring_gather_utils.hpp"
 
 void kernel_main() {
-    // Compile Time Arg(s)
-    constexpr ShardType shard_type = static_cast<ShardType>(get_compile_time_arg_val(0));
-
+    DPRINT << "RR: START\n";
+    DPRINT << "RR: \tmy_y:" << (uint32_t)(my_y[0]) << "\n";
+    DPRINT << "RR: \tmy_x:" << (uint32_t)(my_x[0]) << "\n";
     // TODO: Update the interleaver receive reader kernel invocation to just be able to use this
+    constexpr ShardType shard_type = static_cast<ShardType>(get_compile_time_arg_val(0));
     ShardAddrGen<shard_type> output_tensor_shard_writer;
 
     // Info about the eth receiver eth core (producer of this core)
@@ -44,27 +46,44 @@ void kernel_main() {
     constexpr uint32_t cb_id_in0 = tt::CB::c_in0;
 
     // -1 because we are not receiving from our local chip (local chip has num_dest_cores_worth of tiles already handled)
+    // uint32_t total_num_shards = (output_tensor_shard_writer.get_chunks_per_core_before_advance() - 1) * output_tensor_shard_writer.get_num_dest_cores();
     uint32_t shards_per_ring_index = output_tensor_shard_writer.get_num_dest_cores();
+    DPRINT << "RR: shards_per_ring_index: " << shards_per_ring_index << "\n";
+    // DPRINT << "RR: total_num_shards: " << total_num_shards << "\n";
+    DPRINT << "RR: input_shards_per_eth_buffer: " << input_shards_per_eth_buffer << "\n";
+    DPRINT << "RR: num_transfers: " << input_shards_per_eth_buffer << "\n";
 
     uint32_t const shard_size = output_tensor_shard_writer.get_shard_size_in_bytes();
 
     for (uint32_t t = 0; t < num_transfers; t++) {
+        // DPRINT << "RR: Eth buffer flush " << i << "\n";
         for (uint32_t i = 0; i < shards_per_ring_index; i += input_shards_per_eth_buffer) {
             uint32_t shards_to_send = std::min(input_shards_per_eth_buffer, shards_per_ring_index - i);
             // `shards_to_send` to CB ... need to make sure we are aware of CB wraparound
+            DPRINT << "RR: Waiting for semaphore inc at " << (uint32_t)receiver_read_semaphore_addr_ptr << "\n";
             noc_semaphore_wait(receiver_read_semaphore_addr_ptr, 1);
             noc_semaphore_set(receiver_read_semaphore_addr_ptr, 0);
+            DPRINT << "RR: \tgot semaphore inc from EDM\n";
             for (uint32_t s = 0; s < shards_to_send; ++s) {
+                DPRINT << "RR: Shard " << i + s << "\n";
+                // DPRINT << "\tRR: got signal from EDM \n";
                 // Read page by page so that writer can be kicked off instead of being blocked waiting for full
                 // chunk to be read Look into perf/optimizations for this
                 uint64_t source_eth_buffer_noc_addr = eth_receiver_l1_base_noc_addr + s * shard_size;
+                DPRINT << "RR: \tfetching chunk from source_eth_buffer_noc_addr: " << (uint64_t)(source_eth_buffer_noc_addr & 0xFFFFFFFF) << "\n";
                 fetch_chunk_sharded(
                     cb_id_in0,
                     1,
                     shard_size,
                     source_eth_buffer_noc_addr);
+                // eth_receiver_l1_base_noc_addr += output_tensor_shard_writer.get_shard_size_in_bytes();
+                // DPRINT << "\tRR: fetched chunk EDM \n";
             }
             noc_semaphore_inc(eth_receiver_l1_semaphore_noc_addr, 1);
+
         }
     }
+
+
+    DPRINT << "RR: END\n";
 }
--- tt_eager/tt_dnn/op_library/all_gather/kernels/dataflow/worker_sharded_ring_gather_send_writer.cpp
+++ tt_eager/tt_dnn/op_library/all_gather/kernels/dataflow/worker_sharded_ring_gather_send_writer.cpp
@@ -5,9 +5,11 @@
 #include <cstdint>
 
 #include "dataflow_api.h"
+#include "debug/dprint.h"
 #include "tt_eager/tt_dnn/op_library/all_gather/kernels/dataflow/worker_ring_gather_utils.hpp"
 
 void kernel_main() {
+    DPRINT << "SW: START\n";
     constexpr ShardType shard_type = static_cast<ShardType>(get_compile_time_arg_val(0));
 
     uint32_t arg_index = 0;
@@ -38,13 +40,20 @@ void kernel_main() {
 
     // one input shard per core for the local chip forward to output tensor
     const uint32_t num_input_shards_from_local_ring_index = addr_gen.get_num_dest_cores();
+    // const uint32_t num_input_tensor_shards =
+    //     addr_gen.get_chunks_per_core_before_advance() * addr_gen.get_num_dest_cores();
     const uint32_t shard_size = addr_gen.get_shard_size_in_bytes();
+    DPRINT << "SW: num_input_shards_from_local_ring_index " << num_input_shards_from_local_ring_index << "\n";
     for (uint32_t i = 0; i < num_input_shards_from_local_ring_index; i += shards_per_eth_l1_buffer) {
         uint32_t num_shards_to_send = std::min(shards_per_eth_l1_buffer, num_input_shards_from_local_ring_index - i);
         noc_semaphore_wait(writer_send_semaphore_addr_ptr, 1);
         noc_semaphore_set(writer_send_semaphore_addr_ptr, 0);
         for (uint32_t c = 0; c < num_shards_to_send; c++) {
+            DPRINT << "SW: send_and_write sharded chunk " << c << "\n";
+            // TODO: Might be better to split this?
+            // uint64_t eth_l1_dest_noc_addr = eth_l1_sender_base_noc_addr;
             uint64_t eth_l1_dest_noc_addr = eth_l1_sender_base_noc_addr + shard_size * c;
+            DPRINT << "SW: \twrite_and_send_chunk_sharded to " << (uint32_t)(eth_l1_dest_noc_addr & 0xFFFFFFFF) << "\n";
             write_and_send_chunk_sharded(
                 cb_id_in0, addr_gen, 1 /*1 page == 1 shard for this call*/,
                 eth_l1_dest_noc_addr);
@@ -52,17 +61,30 @@ void kernel_main() {
         noc_semaphore_inc(eth_l1_sender_semaphore_addr, 1);
     }
 
+    DPRINT << "SW: Done Write and send sharded\n";
+
     // num_transfers = num_devices - 1
     for (uint32_t t = 1; t < num_transfers; ++t) {
+        DEBUG_STATUS('S', 'W', 'L', 'S');
+        DPRINT << "SW: Transfer " << t << "\n";
+
         for (uint32_t i = 0; i < num_input_shards_from_local_ring_index; i += shards_per_eth_l1_buffer) {
             uint32_t num_shards_to_send = std::min(shards_per_eth_l1_buffer, num_input_shards_from_local_ring_index - i);
             noc_semaphore_wait(writer_send_semaphore_addr_ptr, 1);
             noc_semaphore_set(writer_send_semaphore_addr_ptr, 0);
             for (uint32_t c = 0; c < num_shards_to_send; ++c) {
+                DEBUG_STATUS('S', 'W', 'L', 'C');
+                DPRINT << "SW: chunk " << c << "\n";
+                DEBUG_STATUS('S', 'W', 'S', 'C');
                 uint64_t eth_l1_dest_noc_addr = eth_l1_sender_base_noc_addr + shard_size * c;
+                DPRINT << "SW: send_chunk_sharded to " << eth_l1_dest_noc_addr << "\n";
                 send_chunk_sharded(cb_id_in0, 1, shard_size, eth_l1_dest_noc_addr);
+                DEBUG_STATUS('S', 'W', 'S', 'c');
             }
+            DPRINT << "SW: semaphore_inc\n";
             noc_semaphore_inc(eth_l1_sender_semaphore_addr, 1);
+            DEBUG_STATUS('S', 'W', 'S', 'i');
         }
     }
+    DPRINT << "SW: END\n";
 }
