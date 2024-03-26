e331693f45d61241d0c494954769a9055385bd91
Mariko Tatsumi <mtatsumi@tenstorrent.com>
2024-03-26 19:10:43 +0000

    #0: test

--- tests/scripts/run_performance.sh
+++ tests/scripts/run_performance.sh
@@ -73,6 +73,8 @@ run_perf_models_cnn_javelin() {
 
     echo "There are no CNN tests for Javelin yet specified. Arch $tt_arch requested"
 
+    env pytest models/experimental/functional_stable_diffusion/demo -m $test_maker
+
     ## Merge all the generated reports
     env python models/perf/merge_perf_results.py
 }
